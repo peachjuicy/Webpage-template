7bf0fbc563f8db66a71732c1c739fb8b8647274e
Kristina Prezelj <kristinathedeveloper@gmail.com>
2020-12-01 11:32:01 +0100

    installed needed modules and plugins, set up folder structure

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+gulp.task('sass', function(){
+  return gulp.src('app/scss/styles.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('app/css'))
+});
