7eea3a9c4d0b4804b13036375308cbf57eceeb4e
Kristina Prezelj <kristinathedeveloper@gmail.com>
2020-12-04 18:18:08 +0100

    set up gulp and browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,66 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-gulp.task('sass', function(){
-  return gulp.src('app/scss/styles.scss')
-    .pipe(sass()) // Converts Sass to CSS with gulp-sass
-    .pipe(gulp.dest('app/css'))
-});
+//initialize modules
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const replace = require('gulp-replace');
+const browserSync = require('browser-sync').create();
+
+// file path vars
+const files = {
+  scssPath: 'app/scss/**/*.scss',
+  jsPath: 'app/js/**/*.js',
+  htmlPath: './*.html'
+}
+
+//sass task to compile scss to css
+function scssTask () {
+  return gulp.src(files.scssPath)
+  .pipe(sourcemaps.init())
+  .pipe(sass())
+  .pipe(postcss([autoprefixer(), cssnano()]))
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('distribution'))
+  .pipe(browserSync.stream())
+}
+
+// JS task 
+
+function jsTask() {
+  return gulp.src(files.jsPath)
+  .pipe(concat('all.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('distribution'))
+  .pipe(browserSync.stream())
+}
+//cachebusting task
+const cbString=new Date().getTime();
+function cacheBustTask() {
+  return gulp.src(['index.html'])
+  .pipe(replace(/cb=\d+/g, 'cb'+cbString))
+  .pipe(gulp.dest('.'))
+}
+
+// watch task
+function watchTask() {
+  browserSync.init({
+		server: {
+      baseDir:"./"
+    }
+	});
+  gulp.watch([files.scssPath, files.jsPath],
+    gulp.parallel(scssTask, jsTask));
+    gulp.watch(files.htmlPath).on("change", browserSync.reload);
+
+}
+//default task
+
+exports.default=gulp.series(
+  gulp.parallel(scssTask, jsTask), 
+  cacheBustTask,
+  watchTask
+  );
